0ffb2940b132de3b184e75272e42678d390ea64b
Olira1 <oliratesgera1@gmail.com>
2026-06-09 12:07:17 +0300

    function

---
+++ Phase 1; Dart_Programming_Fundamentals/function.dart
@@ -0,0 +1,48 @@
+// // function inside the main or scope main qofa kesatti kan fayyadamu dandenyu
+
+// void main(){
+//     void greet(){
+//         print('greet');
+//     }
+//     greet();
+// }
+
+
+// function that written outside or global function
+
+// void greet(){
+//     print('greet');
+// }
+
+// void main(){
+//     greet();
+// }
+
+// // Function with Parameters
+
+// void main(){
+//     void greet(String name){
+//         print("my name is" `$name`);
+//         }
+//     greet('Olira');
+// }
+
+// // Return Values
+
+// void greet(int a, int b){  // void doesn't have a return
+//     int c = a + b;
+//     print(c);
+// }
+
+// void main(){
+//     greet(4,6);
+// }
+
+
+// int greet(int a, int b){
+//     return(a + b);
+// }
+
+// void main(){
+//     print(greet(7,7));
+// }
