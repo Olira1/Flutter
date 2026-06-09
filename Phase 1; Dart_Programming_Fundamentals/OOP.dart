e433c28db9ff74f2882d3012dc794fc31eafac65
Olira1 <oliratesgera1@gmail.com>
2026-06-09 15:21:54 +0300

    OOP without consturctor

---
+++ Phase 1; Dart_Programming_Fundamentals/OOP.dart
@@ -0,0 +1,16 @@
+// // Class
+// // Blueprint for creating objects.
+
+class Person{
+    String name = '';
+    int age = 0;
+}
+
+void main(){
+    Person s = Person();
+
+    s.name = 'Olira';
+    s.age = 22;
+
+    print(s.name);
+}
