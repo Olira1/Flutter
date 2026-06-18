fb402092f88667237972243d4fdbfbd7ff401442
Olira1 <oliratesgera1@gmail.com>
2026-06-18 20:42:35 +0300

    integrate bio text area with dynamic character wrapping

--- Phase 2_Flutter_Fundamentals/hello_flutter/lib/widgets/action_buttons_section.dart
+++ Phase 2_Flutter_Fundamentals/hello_flutter/lib/widgets/action_buttons_section.dart
@@ -43,7 +43,7 @@ class ActionButtonsSection extends StatelessWidget {
                 onPressed: () => _handleHirePress(context),
                 icon: const Icon(Icons.work_outline),
                 label: const Text(
-                  'add statistics section (following, followers, posts) to card',
+                  'integrate bio text area with dynamic character wrapping',
                   style: TextStyle(fontWeight: FontWeight.w500),
                 ),
                 style: OutlinedButton.styleFrom(
