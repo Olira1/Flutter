7f61d2e2b8a2ed9052221a5a582f01a70abd9462
Olira1 <oliratesgera1@gmail.com>
2026-06-18 20:43:53 +0300

    add settings popup menu to top-right corner of card

--- Phase 2_Flutter_Fundamentals/hello_flutter/lib/widgets/action_buttons_section.dart
+++ Phase 2_Flutter_Fundamentals/hello_flutter/lib/widgets/action_buttons_section.dart
@@ -43,7 +43,7 @@ class ActionButtonsSection extends StatelessWidget {
                 onPressed: () => _handleHirePress(context),
                 icon: const Icon(Icons.work_outline),
                 label: const Text(
-                  'implement follow button with toggleable state logic',
+                  'add settings popup menu to top-right corner of card',
                   style: TextStyle(fontWeight: FontWeight.w500),
                 ),
                 style: OutlinedButton.styleFrom(
