da96ad6bc39f10c1919b4ff44530049f0704c658
Olira1 <oliratesgera1@gmail.com>
2026-06-19 19:19:04 +0300

    inserting everthing in the profile section

--- Business_Card_kan_koo/businesscard/lib/Widget/profile_header.dart
+++ Business_Card_kan_koo/businesscard/lib/Widget/profile_header.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 
 class ProfileHeader extends StatelessWidget {
 
-  final String title;
-
-
   const ProfileHeader({
     super.key,
-    required this.title,
     });
 
   @override
@@ -15,11 +11,41 @@ class ProfileHeader extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
+        CircleAvatar(
+          radius: 60,
+           backgroundColor: Colors.blueAccent,
+          child:const Icon(
+            Icons.person,
+            color: Colors.black,
+            size: 60,
+          ),
+        ),
+        
+        SizedBox(height: 6),
+
         Text(
-          title,
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20,),
-        )
+          'Olira Tesgera',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+        ),
+
+        SizedBox(height: 6),
+
+        Text(
+          'Senior Flutter Developer',
+          style: TextStyle(color: Colors.green, fontSize: 20),
+        ),
+
+        SizedBox(height: 6),
+
+        Padding(
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+          child: Text(         
+             'Passionate about creating beautiful and functional mobile apps with Flutter. 5+ years of experience in mobile development.',
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300, height: 1.4),)
+
+        ),
+
+        SizedBox(height: 10),
         
       ],
     );
--- Business_Card_kan_koo/businesscard/lib/screens/business_card_screen.dart
+++ Business_Card_kan_koo/businesscard/lib/screens/business_card_screen.dart
@@ -4,7 +4,6 @@ import '../Widget/contact_info_item.dart';
 import '../Widget/social_media_section.dart';
 import '../Widget/action_buttons_section.dart';
 
-
 class BusinessCardScreen extends StatelessWidget {
   const BusinessCardScreen({super.key});
 
@@ -15,27 +14,25 @@ class BusinessCardScreen extends StatelessWidget {
         title: const Text('Business Card'),
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
+      ),
 
-        body: const SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
 
+          child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                ProfileHeader(
-                  title: 'your profile will be here',
-                ),
+                ProfileHeader(),
                 ContactInfoItem(title: 'your contact will be here'),
                 SocialMediaSection(title: 'your contact will be here'),
-                ActionButtonsSection(title: 'Your action will be here',),
+                ActionButtonsSection(title: 'Your action will be here'),
               ],
             ),
-          )
-
-          
+          ),
         ),
+      ),
     );
   }
 }
